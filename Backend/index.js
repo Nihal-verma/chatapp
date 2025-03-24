73dc13a1dd6a94c916c3d7807d2f9d982da07a02
Nihal-verma <nihal1999verma@gmail.com>
2025-03-24 14:53:59 +0530

    third commiting

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit"); 
+const { query, validationResult } = require('express-validator');
 
 const PORT = 3000;
 const app = express();
@@ -16,6 +18,13 @@ const io = new Server(server, {
     credentials: true,
   },
 });
+
+const postRateLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 10,
+  message: { success: false, message: "Too many requests, please try again later." }
+});
+
 
 io.on("connection", (socket) => {
   console.log("User Connected:", socket.id);
@@ -33,9 +42,14 @@ io.on("connection", (socket) => {
 
 app.use(cors());
 
+app.use(postRateLimiter) // can pass it as app level middleware or can use it in route for each api
 app.get("/", (req, res) => {
   res.json("Hello, server is running.");
 });
+app.post("/create", (req, res) => {
+  let data = req.body;
+  res.json({ message: "Message received", success: true, data });
+})
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
